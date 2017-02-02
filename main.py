2e786a4da36088126dbbbdd68c1077c60584a381
Jameson <jmarsenault@email.neit.edu>
2017-02-02 13:00:57 -0500

    added form, and comments on what else to add

--- main.py
+++ main.py
@@ -15,6 +15,82 @@
 # limitations under the License.
 #
 import webapp2
+import re
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jameson's User-Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>User Signup</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+# User signup form
+form = """
+    <form action="/" method="post">
+        <table>
+            <tr>
+                <td>
+                    <label for="username">Username</label>
+                </td>
+                <td>
+                    <input type="text" required name="username" value="%(username)s" />
+                    <span class="error">%(username_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="password">Password</label>
+                </td>
+                <td>
+                    <input type="password" required name="password" />
+                    <span class="error">%(password_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="passwordConfirm">Confirm Password</label>
+                </td>
+                <td>
+                    <input type="password" required name="passwordMatch" />
+                    <span class="error">%(passwordMatch_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email (optional)</label>
+                </td>
+                <td>
+                    <input type="email" name="email" value="%(email)s"/>
+                    <span class="error">%(email_error)s</span>
+                </td>
+            </tr>
+        </table>
+        <input type="submit" value="Sign Up!" />
+    </form>
+"""
+
+# reg expressions for username, password, email
+
+# defining what a valid username is
+
+# defining what a valid password is
+
+# defining what a valid email is
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
