8d15341a3f517e40e1ae9418ba1760cffd1eda26
Jameson <jmarsenault@email.neit.edu>
2017-02-02 14:30:55 -0500

    added regular expressions, and defined them, added creation of the form and set variables in mainhandler

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ page_footer = """
 """
 
 # User signup form
-form = """
+signup_form = """
     <form action="/" method="post">
         <table>
             <tr>
@@ -66,8 +66,8 @@ form = """
                     <label for="passwordConfirm">Confirm Password</label>
                 </td>
                 <td>
-                    <input type="password" required name="passwordMatch" />
-                    <span class="error">%(passwordMatch_error)s</span>
+                    <input type="password" required name="passwordConfirmed" />
+                    <span class="error">%(passwordConfirmed_error)s</span>
                 </td>
             </tr>
             <tr>
@@ -84,17 +84,43 @@ form = """
     </form>
 """
 
+
+# putting the page together
+content = page_header + signup_form + page_footer
+
 # reg expressions for username, password, email
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
 
 # defining what a valid username is
+def is_valid_username(username):
+    return USER_RE.match(username)
+
 
 # defining what a valid password is
+def is_valid_password(password):
+    return PASS_RE.match(password)
+
 
 # defining what a valid email is
+def is_valid_email(email):
+    return EMAIL_RE.match(email)
+
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, username_error="", password_error="", passwordConfirmed_error="", email_error="", username="",
+                   email=""):
+        self.response.out.write(content % {"username_error": username_error,
+                                        "password_error": password_error,
+                                        "passwordConfirmed_error": passwordConfirmed_error,
+                                        "email_error": email_error,
+                                        "username": username,
+                                        "email": email})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
